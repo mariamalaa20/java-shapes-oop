c75edf0863bdd8b48e905582eb889f8e5b395ae5
mariamalaa20 <159918273+mariamalaa20@users.noreply.github.com>
2025-04-14 10:00:01 +0300

    Add files via upload

---
+++ Class1.java
@@ -0,0 +1,16 @@
+public class Class1 implements IInterface {
+    @Override
+    public void method1() {
+        // Not implemented
+    }
+
+    @Override
+    public String methodReturningString() {
+        return null;
+    }
+
+    @Override
+    public int methodReturningInt(int num1, int num2) {
+        return 0;
+    }
+}
