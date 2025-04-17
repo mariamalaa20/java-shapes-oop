958547e2ded99df981ae80cc0d3f4b43ca3c80cf
mariamalaa20 <159918273+mariamalaa20@users.noreply.github.com>
2025-04-17 13:37:06 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ICalculations circle = new Circle();
+        circle.calculatePerimeter();
+
+        Circle circle1 = new Circle();
+        circle1.arcCalculation();
+    }
+}
