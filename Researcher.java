9698ffaa90c704f2812152ff081c1ebe0035a7fa
mariamalaa20 <159918273+mariamalaa20@users.noreply.github.com>
2025-04-19 22:13:14 +0300

    Add files via upload

---
+++ Researcher.java
@@ -0,0 +1,11 @@
+public class Researcher implements IResearcher {
+    @Override
+    public void workingHours() {
+        // Not implemented
+    }
+
+    @Override
+    public void accessPermission() {
+        // Not implemented
+    }
+}
