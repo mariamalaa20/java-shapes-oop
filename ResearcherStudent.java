0505d4a8d0c9f01d53d619011f329d6f295dcf42
mariamalaa20 <159918273+mariamalaa20@users.noreply.github.com>
2025-04-20 14:18:54 +0300

    Add files via upload

---
+++ ResearcherStudent.java
@@ -0,0 +1,21 @@
+public class ResearcherStudent implements IResearcher, IStudent {
+    @Override
+    public void workingHours() {
+        // Not implemented
+    }
+
+    @Override
+    public void accessPermission() {
+        // Not implemented
+    }
+
+    @Override
+    public void department() {
+        // Not implemented
+    }
+
+    @Override
+    public void grade() {
+        // Not implemented
+    }
+}
